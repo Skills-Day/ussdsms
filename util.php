287449f7ce3f7f62d6726271b76521c31cfe95bb
Derdus <derduskenga@gmail.com>
2021-04-15 15:13:09 +0300

    Add files via upload

---
+++ util.php
@@ -0,0 +1,42 @@
+<?php
+    class Util{
+        //DB variable 
+		static $DB_NAME = "";
+		static $DB_USER = "";
+		static $DB_USER_PASS = "";
+		static $SERVER_NAME = "";
+
+        //About USSD menu
+        static $GO_BACK = "98";
+        static $GO_TO_MAIN_MENU = "99";
+
+        //user intial balance 
+        static $USER_BALANCE = ;
+
+        static $COUNTRY_CODE = "";
+
+        static $TRANSACTION_FEE = ;
+
+        //static  $API_KEY = ""; //sandbox
+        static  $API_KEY = ""; //prod
+
+        //static $API_USERNAME = ""; //sandbox
+        static $API_USERNAME = ""; //prod
+
+        //static $SMS_SHORTCODE = "";//sandbox
+        static $SMS_SHORTCODE = "";//prod.
+        static $SMS_SHORTCODE_KEYWORD = ""; //prod.
+        static $COMPANY_NAME = " ";
+    }
+?>
+
+
+
+
+
+
+
+
+
+
+
